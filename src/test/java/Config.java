ad8ad9eae8fb12cba86c2e94ec6274e812b9463c
Petar Aleksandrov <petar.aleksandrov@musala.com>
2019-11-25 16:37:55 +0200

    add Config.java. add new test for logout in LoginFormTests.java

---
+++ src/test/java/Config.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+
+public class Config {
+
+    public final static String USERNAME = "tomsmith";
+    public final static String PASSWORD = "SuperSecretPassword!";
+
+    public enum Browsers {
+        Chrome,
+        FireFox
+    }
+
+}
